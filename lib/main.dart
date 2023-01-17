4e63a9d3bb1130a629d3876557e6f7cbf1010146
Shubham-Ghoshh <87618625+Shubham-Ghoshh@users.noreply.github.com>
2023-01-17 18:41:28 +0530

    \

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:get/get.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'package:health_studio_user/core/bindings.dart';
 import 'package:health_studio_user/ui/screens/logged_in_home_screen.dart';
 import 'package:health_studio_user/ui/screens/splash_screen.dart';
--- lib/ui/screens/authentication/login_screen.dart
+++ lib/ui/screens/authentication/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:health_studio_user/core/controllers/auth_controller.dart';
 import 'package:health_studio_user/utils/spacing.dart';
