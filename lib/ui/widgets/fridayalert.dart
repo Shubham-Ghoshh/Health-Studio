543cbc1ca3da90c216ff45540efe8ce38560f807
Sakina Boriwala <sakina.boriwala21@gmail.com>
2023-03-10 18:35:35 +0530

    translations added

--- lib/ui/widgets/fridayalert.dart
+++ lib/ui/widgets/fridayalert.dart
@@ -6,28 +6,28 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../utils/colors.dart';
 
 Future fridayAlertDialog() {
-    return showDialog(
-        context: navigatorKey.currentContext!,
-        builder: (context) {
-          return AlertDialog(
-            icon: const Icon(
-              Icons.error,
-              color: loginButtonColor,
+  return showDialog(
+      context: navigatorKey.currentContext!,
+      builder: (context) {
+        return AlertDialog(
+          icon: const Icon(
+            Icons.error,
+            color: loginButtonColor,
+          ),
+          title: Text(AppLocalizations.of(context)!.info),
+          content: Text(AppLocalizations.of(context)!.friday_alert),
+          actionsAlignment: MainAxisAlignment.center,
+          actions: <Widget>[
+            LoginButton(
+              onTap: () {
+                Get.back();
+              },
+              enabled: true,
+              title: AppLocalizations.of(context)!.okay,
+              height: 50,
+              width: 100,
             ),
-            title: Text(AppLocalizations.of(context)!.info),
-            content: Text("On Friday, orders are not deliverable"),
-            actionsAlignment: MainAxisAlignment.center,
-            actions: <Widget>[
-              LoginButton(
-                onTap: () {
-                  Get.back();
-                },
-                enabled: true,
-                title: AppLocalizations.of(context)!.okay,
-                height: 50,
-                width: 100,
-              ),
-            ],
-          );
-        });
-  }
+          ],
+        );
+      });
+}
